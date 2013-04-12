f2a878280892487fdf824912827e87edc428ae9b
sanemat <o.gata.ken@gmail.com>
2013-04-12 10:01:51 +0900

    Add initial test
    see reed/actionview-encoded_mail_to

---
+++ test/test_actionview-link_to_blank.rb
@@ -0,0 +1,13 @@
+require 'action_pack'
+require 'action_view/helpers/url_helper'
+require 'action_view/link_to_blank/link_to_blank'
+
+class TestActionViewLinkToBlank < MiniTest::Unit::TestCase
+  include ActionView::Helpers::UrlHelper
+
+  def test_initialization
+    [:link_to_blank].each do |method|
+      assert_includes ActionView::Helper::UrlHelper.instance_methods, method
+    end
+  end
+end
